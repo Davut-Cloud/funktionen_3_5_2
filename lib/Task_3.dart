a44e51cefdd246751f80aca8b2103c16328033ff
Davut <davesizzle@googlemail.com>
2025-03-12 11:31:08 +0100

    add commengt

--- lib/Task_3.dart
+++ lib/Task_3.dart
@@ -1,5 +1,8 @@
+//Funktion die  den jeweils letzten Buchstaben, gefolgt von einem punkt ausgibt
+
 void reverseInitials(String firstName, String lastName) {
-  String firstInitial = firstName[firstName.length - 1];
+  String firstInitial = firstName[firstName.length -
+      1]; //firstName[firstName.length - 1].toUpperCase(); für Ausgabe in Großbuchstaben
   // 0-basierte Indexierung und das letze Zeichen darum bei firstName.length - 1 zu finden!!!
 
   String lastInitial = lastName[lastName.length - 1];
