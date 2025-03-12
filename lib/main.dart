808d326368f93af0e193726cb3aa1a73f570f28e
Davut <davesizzle@googlemail.com>
2025-03-12 10:47:02 +0100

    first init

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+// Funktion, die eine Zahl ausgibt
+void printNumber(int number) {
+  print("Die Zahl ist: $number");
+}
+
+void main() {
+  printNumber(5);
+  printNumber(42);
+  printNumber(100);
+}
